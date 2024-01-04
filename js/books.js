ae7a1085dc6954cf3ac2beaf85695a777f7a36ee
Taulant Qela <taulantqela@gmail.com>
2024-01-04 23:02:04 +0100

    refactor book filtering

--- js/books.js
+++ js/books.js
@@ -1,31 +1,31 @@
 var books = [
     {
-      image: 'https://s-media-cache-ak0.pinimg.com/564x/f9/8e/2d/f98e2d661445620266c0855d418aab71.jpg',
+      image: "http://d.gr-assets.com/books/1414348859l/23209971.jpg",
       author: 'Mary Shelly',
-      title: 'Andi',
+      title: 'Libri  1',
       price: 34
     },
     {
       image: 'https://s-media-cache-ak0.pinimg.com/564x/f9/8e/2d/f98e2d661445620266c0855d418aab71.jpg',
-      author: 'Mary Shelly',
+      author: 'Libri 2',
       title: 'Nita',
       price: 34
     },
     {
-      image: 'https://s-media-cache-ak0.pinimg.com/564x/f9/8e/2d/f98e2d661445620266c0855d418aab71.jpg',
+      image: "http://prodimage.images-bn.com/pimages/9780062315007_p0_v2_s192x300.jpg",
       author: 'Mary Shelly',
       title: 'Taulant',
       price: 34
     },
     {
-      image: 'https://s-media-cache-ak0.pinimg.com/564x/f9/8e/2d/f98e2d661445620266c0855d418aab71.jpg',
+      image: "https://s-media-cache-ak0.pinimg.com/564x/f9/8e/2d/f98e2d661445620266c0855d418aab71.jpg",
       author: 'Mary Shelly',
-      title: 'Test test',
+      title: 'Libri 3',
       price: 34
     }
   ]
 
-  this.populateDom(books)
+  populateDom(books)
   
   function populateDom(books) {
       var output = '';
@@ -39,19 +39,20 @@ var books = [
                                          "</div>" + 
                                        "</div>"
                     )
+
       document.getElementById('list-th').innerHTML = output;
   }
 
-  function getContent() {
+  function filterBooks() {
     // Get the value from the input field
-    var search = document.getElementById("textInput").value;
+    var search = document.getElementById("textInput").value.toLowerCase();
     
     if(search != '') {
-      var filteredBooks = books.filter(book => book.title.toLowerCase().startsWith(search.toLowerCase()))
-      this.populateDom(filteredBooks)
+      var filteredBooks = books.filter(book => book.title.toLowerCase().startsWith(search))
+      populateDom(filteredBooks)
 
       return 
     }
 
-    this.populateDom(this.books)
+    populateDom(books)
   }
